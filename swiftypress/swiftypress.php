bbb2c0998671973167a43fdd0d00b9c5705aec39
Basem Emara <contact@basememara.com>
2019-05-24 08:35:35 -0400

    Fix SVN trunk

--- swiftypress/swiftypress.php
+++ swiftypress/swiftypress.php
@@ -3,7 +3,7 @@
    Plugin Name: SwiftyPress
    Plugin URI:  http://zamzam.io
    Description: Mobile app framework for your WordPress blog.
-   Version:     1.5.0
+   Version:     1.5.1
    Author:      zamzaminc
    Author URI:  http://zamzam.io
    Text Domain: swiftypress
